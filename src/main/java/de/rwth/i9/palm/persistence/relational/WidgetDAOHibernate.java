912a35ef60f76a21a69a83a49eb9dc71ca8d600f
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-05-18 03:41:36 +0200

    changed Widget DAO inplementation method name and fixed query

--- src/main/java/de/rwth/i9/palm/persistence/relational/WidgetDAOHibernate.java
+++ src/main/java/de/rwth/i9/palm/persistence/relational/WidgetDAOHibernate.java
@@ -1,16 +1,70 @@
 package de.rwth.i9.palm.persistence.relational;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 
 import de.rwth.i9.palm.model.Widget;
+import de.rwth.i9.palm.model.WidgetType;
+import de.rwth.i9.palm.persistence.InstantiableDAO;
 import de.rwth.i9.palm.persistence.WidgetDAO;
 
-public class WidgetDAOHibernate extends GenericDAOHibernate<Widget> implements WidgetDAO
+public class WidgetDAOHibernate extends GenericDAOHibernate<Widget> implements WidgetDAO, InstantiableDAO
 {
 
 	public WidgetDAOHibernate( SessionFactory sessionFactory )
 	{
 		super( sessionFactory );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public List<Widget> getActiveWidgetByWidgetType( WidgetType widgetType )
+	{
+		StringBuilder queryString = new StringBuilder();
+		queryString.append( "FROM Widget " );
+		queryString.append( "WHERE widgetType = :widgetType " );
+		queryString.append( "AND widgetStatus = 'ACTIVE'" );
+
+		Query query = getCurrentSession().createQuery( queryString.toString() );
+		query.setParameter( "widgetType", widgetType );
+
+		@SuppressWarnings( "unchecked" )
+		List<Widget> widgets = query.list();
+
+		if ( widgets == null || widgets.isEmpty() )
+			return Collections.emptyList();
+
+		return widgets;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public List<Widget> getActiveWidgetByWidgetTypeAndGroup( WidgetType widgetType, String widgetGroup )
+	{
+		StringBuilder queryString = new StringBuilder();
+		queryString.append( "FROM Widget " );
+		queryString.append( "WHERE widgetType = :widgetType " );
+		queryString.append( "AND widgetGroup = :widgetGroup " );
+		queryString.append( "AND widgetStatus = 'ACTIVE'" );
+
+		Query query = getCurrentSession().createQuery( queryString.toString() );
+		query.setParameter( "widgetType", widgetType );
+		query.setParameter( "widgetGroup", widgetGroup );
+
+		@SuppressWarnings( "unchecked" )
+		List<Widget> widgets = query.list();
+
+		if ( widgets == null || widgets.isEmpty() )
+			return Collections.emptyList();
+
+		return widgets;
 	}
 
 }
